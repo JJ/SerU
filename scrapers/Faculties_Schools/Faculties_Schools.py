7df12aaf1ae8af77aae400fcde38211b7bb68d23
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-08-10 00:05:24 +0200

    Modificada la estructura

--- scrapers/Faculties_Schools/Faculties_Schools.py
+++ scrapers/Faculties_Schools/Faculties_Schools.py
@@ -16,41 +16,56 @@ REQUEST_LINK_FACULTIES = "https://www.ugr.es/pages/centros/facultades"
 UNIVERSITY_LINK = 'https://www.ugr.es'
 REQUEST_LINK_SCHOOLS = "https://www.ugr.es/pages/centros/escuelas"
 
-# Obtain faculties names and hrefs
+# Returns tuple with the schools, faculties and links
+def obtainFacultiesSchools():
 
-faculties_page = requests.get(REQUEST_LINK_FACULTIES)
-faculties_soup = BeautifulSoup(faculties_page.content, 'html.parser')
-faculty_obj_list = faculties_soup.find_all(class_='wikilink2')
-faculties = []
-faculties_href = []
-for obj in faculty_obj_list:
-    faculties_href.append(UNIVERSITY_LINK + obj.get('href').lstrip())
-    faculties.append(obj.get_text().lstrip())
+    # Obtain faculties names and hrefs
 
-# Obtain schools names and hrefs
+    faculties_page = requests.get(REQUEST_LINK_FACULTIES)
+    faculties_soup = BeautifulSoup(faculties_page.content, 'html.parser')
+    faculty_obj_list = faculties_soup.find_all(class_='wikilink2')
+    faculties = []
+    faculties_href = []
+    for obj in faculty_obj_list:
+        faculties_href.append(UNIVERSITY_LINK + obj.get('href').lstrip())
+        faculties.append(obj.get_text().lstrip())
 
-schools_page = requests.get(REQUEST_LINK_SCHOOLS)
-schools_soup = BeautifulSoup(schools_page.content, 'html.parser')
-schools_obj_list = schools_soup.find_all(class_="wikilink2")
-schools = []
-schools_href = []
-for obj in schools_obj_list:
-    schools.append(obj.get_text().lstrip())
-    schools_href.append(UNIVERSITY_LINK + obj.get('href').lstrip())
+    # Obtain schools names and hrefs
 
-faculties_and_schools = faculties + schools
-faculties_and_schools_href = faculties_href + schools_href
-data = []
+    schools_page = requests.get(REQUEST_LINK_SCHOOLS)
+    schools_soup = BeautifulSoup(schools_page.content, 'html.parser')
+    schools_obj_list = schools_soup.find_all(class_="wikilink2")
+    schools = []
+    schools_href = []
+    for obj in schools_obj_list:
+        schools.append(obj.get_text().lstrip())
+        schools_href.append(UNIVERSITY_LINK + obj.get('href').lstrip())
 
-number_of_elements = len(faculties_and_schools)
-for i in range(number_of_elements):
-    obj = {"name":faculties_and_schools[i], "href":faculties_and_schools_href[i]}
-    data.append(obj)
+    faculties_and_schools = faculties + schools
+    faculties_and_schools_href = faculties_href + schools_href
+    return (faculties_and_schools, faculties_and_schools_href)
 
-json_object = json.dumps(data, ensure_ascii=False)
+# Obtains the JSON object
+def createJSON(faculties_and_schools, faculties_and_schools_href):
 
-# Would be changed soon
+    data = []
+    number_of_elements = len(faculties_and_schools)
+    for i in range(number_of_elements):
+        obj = {"name":faculties_and_schools[i], "href":faculties_and_schools_href[i]}
+        data.append(obj)
 
-sys.stdout.write(json_object)
-sys.stdout.flush()
-sys.exit(0)
+    json_object = json.dumps(data, ensure_ascii=False)
+    return (json_object)
+
+def main():
+    (fac_schools,hr) = obtainFacultiesSchools()
+    json_object = createJSON(fac_schools, hr)
+
+    # Would be changed soon
+
+    sys.stdout.write(json_object)
+    sys.stdout.flush()
+    sys.exit(0)
+
+# Calls main
+main()
