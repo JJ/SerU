972831a43d16d5cb2822f1f2c42f4d20581bbbc3
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-08-09 22:11:31 +0200

    Obtenidos los enlaces y títulos

--- scrapers/agenda_eventos.py
+++ scrapers/agenda_eventos.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import json
 import sys
 
+# Change encoding to UTF8
+reload(sys)
+sys.setdefaultencoding('UTF8')
+
 # Obtain the news objects.
 
 news_channel = requests.get("https://canal.ugr.es/agenda/semana/")
@@ -17,3 +21,10 @@ raw_times = news_channel_soup.find_all(class_="event-schedule-detail text-medium
 
 for element in raw_times:
     times.append(element.get_text().lstrip())
+
+for element in raw_titles_hrefs:
+    hrefs.append(str(element.get('href')).lstrip())
+    titles.append(str(element.get_text()).lstrip())
+
+for element in titles:
+    print(titles)
