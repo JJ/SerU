070f8075c429399f6abfcf03a79da541e56d32ec
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-08-10 00:00:44 +0200

    Añadidas las constantes para los links

--- scrapers/Faculties_Schools/Faculties_Schools.py
+++ scrapers/Faculties_Schools/Faculties_Schools.py
@@ -4,29 +4,6 @@ from bs4 import BeautifulSoup
 import json
 import sys
 
-
-# Obtain faculties names and hrefs
-
-faculties_page = requests.get("https://www.ugr.es/pages/centros/facultades")
-faculties_soup = BeautifulSoup(faculties_page.content, 'html.parser')
-faculty_obj_list = faculties_soup.find_all(class_='wikilink2')
-faculties = []
-faculties_href = []
-for obj in faculty_obj_list:
-    faculties_href.append('https://www.ugr.es' + obj.get('href').lstrip())
-    faculties.append(obj.get_text().lstrip())
-
-# Obtain schools names and hrefs
-
-schools_page = requests.get("https://www.ugr.es/pages/centros/escuelas")
-schools_soup = BeautifulSoup(schools_page.content, 'html.parser')
-schools_obj_list = schools_soup.find_all(class_="wikilink2")
-schools = []
-schools_href = []
-for obj in schools_obj_list:
-    schools.append(obj.get_text().lstrip())
-    schools_href.append('https://www.ugr.es' + obj.get('href').lstrip())
-
 '''
 The definition of the JSON object would be:
 {
@@ -34,6 +11,32 @@ The definition of the JSON object would be:
     href: faculty/school hyperlink,
 };
 '''
+
+REQUEST_LINK_FACULTIES = "https://www.ugr.es/pages/centros/facultades"
+UNIVERSITY_LINK = 'https://www.ugr.es'
+REQUEST_LINK_SCHOOLS = "https://www.ugr.es/pages/centros/escuelas"
+
+# Obtain faculties names and hrefs
+
+faculties_page = requests.get(REQUEST_LINK_FACULTIES)
+faculties_soup = BeautifulSoup(faculties_page.content, 'html.parser')
+faculty_obj_list = faculties_soup.find_all(class_='wikilink2')
+faculties = []
+faculties_href = []
+for obj in faculty_obj_list:
+    faculties_href.append(UNIVERSITY_LINK + obj.get('href').lstrip())
+    faculties.append(obj.get_text().lstrip())
+
+# Obtain schools names and hrefs
+
+schools_page = requests.get(REQUEST_LINK_SCHOOLS)
+schools_soup = BeautifulSoup(schools_page.content, 'html.parser')
+schools_obj_list = schools_soup.find_all(class_="wikilink2")
+schools = []
+schools_href = []
+for obj in schools_obj_list:
+    schools.append(obj.get_text().lstrip())
+    schools_href.append(UNIVERSITY_LINK + obj.get('href').lstrip())
 
 faculties_and_schools = faculties + schools
 faculties_and_schools_href = faculties_href + schools_href
